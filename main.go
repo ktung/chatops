d71fa4a7fe206af8e1c0ead17abbfdf12a7107bc
ktung <ptung@protonmail.ch>
2025-05-11 21:30:38 -0400

    feat: add palserver commands

--- main.go
+++ main.go
@@ -86,7 +86,32 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		output, err := exec.Command("powershell", "-WindowStyle", "Hidden", "-Command", gameServersCmdPath+"enshrouded_info.ps1").Output()
 		if err != nil {
 			fmt.Printf("Error executing enshrouded_info: %s", err.Error())
-			s.ChannelMessageSend(m.ChannelID, "Enshrouded restart error !")
+			s.ChannelMessageSend(m.ChannelID, "Enshrouded info error !")
+			return
+		}
+		s.ChannelMessageSend(m.ChannelID, string(output))
+	}
+
+	if m.Content == "palserver_restart" {
+		gameServersCmdPath := os.Getenv("GAME_SERVERS_CMD_PATH")
+
+		cmd := exec.Command("powershell", "-WindowStyle", "Hidden", "-Command", gameServersCmdPath+"palserver_restart.ps1")
+		err := cmd.Run()
+		if err != nil {
+			fmt.Printf("Error executing palserver_restart: %s", err.Error())
+			s.ChannelMessageSend(m.ChannelID, "Palserver restart error !")
+			return
+		}
+		s.ChannelMessageSend(m.ChannelID, "Palserver restarted !")
+	}
+
+	if m.Content == "palserver_info" {
+		gameServersCmdPath := os.Getenv("GAME_SERVERS_CMD_PATH")
+
+		output, err := exec.Command("powershell", "-WindowStyle", "Hidden", "-Command", gameServersCmdPath+"palserver_info.ps1").Output()
+		if err != nil {
+			fmt.Printf("Error executing palserver_info: %s", err.Error())
+			s.ChannelMessageSend(m.ChannelID, "Palserver info error !")
 			return
 		}
 		s.ChannelMessageSend(m.ChannelID, string(output))
