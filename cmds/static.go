b435d8a01300780a56d2eb396d610c127ac92f6e
ktung <ptung@protonmail.ch>
2025-05-17 23:32:44 -0400

    refactor: remove ping/pong commands

--- cmds/static.go
+++ cmds/static.go
@@ -7,22 +7,6 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
-func PingCommand(s *discordgo.Session, m *discordgo.MessageCreate) error {
-	_, err := s.ChannelMessageSend(m.ChannelID, "Pong!")
-	if err != nil {
-		log.Printf("Error sending Pong message: %v", err)
-	}
-	return err
-}
-
-func PongCommand(s *discordgo.Session, m *discordgo.MessageCreate) error {
-	_, err := s.ChannelMessageSend(m.ChannelID, "Ping!")
-	if err != nil {
-		log.Printf("Error sending Ping message: %v", err)
-	}
-	return err
-}
-
 func NewHelpCommand(router *CommandsMap) CommandHandler {
 	return func(s *discordgo.Session, m *discordgo.MessageCreate) error {
 		helpMessage := "Available commands:\n"
